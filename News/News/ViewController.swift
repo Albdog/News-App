3d00bd24172fce9b0aa4a9df4222abe3ff1e71ea
Joaquin Jacinto <joaquinjacinto11@gmail.com>
2019-08-13 18:48:48 +0800

    initial commit

---
+++ News/News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Joaquin Jacinto on 8/13/19.
+//  Copyright © 2019 Joaquin Jacinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
