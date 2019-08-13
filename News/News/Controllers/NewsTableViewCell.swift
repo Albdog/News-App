efae2e0a3263b0b31178d128cf82ba4e084d33a5
Joaquin Jacinto <joaquinjacinto11@gmail.com>
2019-08-13 19:08:52 +0800

    PBX - added new controllers

---
+++ News/News/Controllers/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  News
+//
+//  Created by Joaquin Jacinto on 8/13/19.
+//  Copyright © 2019 Joaquin Jacinto. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
