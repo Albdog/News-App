946471fd3441932ced7148cf1860badf46456d68
Joaquin Jacinto <joaquinjacinto11@gmail.com>
2019-08-14 13:29:27 +0800

    NewsTableViewController - added logic for handling null from api

--- News/News/Controllers/NewsTableViewController.swift
+++ News/News/Controllers/NewsTableViewController.swift
@@ -17,18 +17,19 @@ class NewsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.loadNewsArticles { (JSON) in }
+        loadNewsArticles { (JSON) in }
     }
 
-    // MARK: - Table view data source
+    @IBAction func reloadPressed(_ sender: Any) {
+        loadNewsArticles { (JSON) in }
+        print("reload pressed")
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return newsArticles.count
     }
 
@@ -40,52 +41,24 @@ class NewsTableViewController: UITableViewController {
         var article = newsArticles[indexPath.row]
         
         cell.newsTitle.text = article["title"] as? String
+        
         if let urlImage = article["urlToImage"] as? String {
             let url = URL(string: urlImage)
             if let data = try? Data(contentsOf: url!) {
                 cell.newsImage.image = UIImage(data: data)
             }
+        } else {
+            
         }
-        cell.newsDescription.text = article["description"] as? String
+        
+        if let description = article["description"] as? String {
+            cell.newsDescription.text = description
+        } else {
+            cell.newsDescription.text = "No description."
+        }
 
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -123,4 +96,5 @@ extension NewsTableViewController {
             completion(swiftyJsonVar)
         }
     }
+    
 }
