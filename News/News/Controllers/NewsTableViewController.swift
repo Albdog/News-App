4c5366c6013c73b5c75f3ede240e57191bb75dd7
Joaquin Jacinto <joaquinjacinto11@gmail.com>
2019-08-14 11:33:51 +0800

    NewsTableViewController - created logic for loading from API and setting cell values

--- News/News/Controllers/NewsTableViewController.swift
+++ News/News/Controllers/NewsTableViewController.swift
@@ -7,40 +7,73 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class NewsTableViewController: UITableViewController {
+    
+    var newsArticles = [[String:AnyObject]]()
 
+    //move to extension
+    func loadNewsArticles(completion: @escaping (JSON?) -> Void) {
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=" + "3be68be40de74b5caba36f3852414f40"
+        
+        Alamofire.request(url).responseJSON { resposeData in
+            guard resposeData.result.isSuccess, let value = resposeData.result.value else {
+                print("Error fetching news articles")
+                completion(nil)
+                return
+            }
+            
+            let swiftyJsonVar = JSON(value)
+            
+            if let resData = swiftyJsonVar["articles"].arrayObject {
+                self.newsArticles = resData as! [[String:AnyObject]]
+            }
+            
+            if self.newsArticles.count > 0 {
+                self.tableView.reloadData()
+            }
+            
+            completion(swiftyJsonVar)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        self.loadNewsArticles { (JSON) in }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return newsArticles.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsViewCell", for: indexPath) as? NewsTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of NewsViewCell.")
+        }
+        
+        var article = newsArticles[indexPath.row]
+        
+        cell.newsTitle.text = article["title"] as? String
+        if let urlImage = article["urlToImage"] as? String {
+            print(urlImage)
+            cell.newsImage.downloaded(from: urlImage)
+        }
+        cell.newsDescription.text = article["description"] as? String
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
